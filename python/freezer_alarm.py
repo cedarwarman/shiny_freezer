b03d5dd888e25a2fff4302dd265cdbe795548009
cedarwarman <cedardalewarman@gmail.com>
2022-09-21 15:48:06 -0700

    Add log to alarm script

--- python/freezer_alarm.py
+++ python/freezer_alarm.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import os
+import time
 import pandas as pd
 from gspread_pandas import Spread
+import yagmail
 
 ### Import sheet
 def import_sheet(sheet_id):
@@ -10,11 +13,58 @@ def import_sheet(sheet_id):
     df['temp_c'] = df['temp_c'].astype(float)
     return(df)
 
+### Open log file
+def open_log_file():
+    log_file_path = os.path.join(os.getcwd(), "python_data", "freezer_alarm_log.tsv")
+    if not os.path.exists(os.path.join(os.getcwd(), "python_data")):
+        os.makedirs(os.path.join(os.getcwd(), "python_data"))
+
+    try:
+        f = open(log_file_path, 'a+')
+        if os.stat(log_file_path).st_size == 0:
+                f.write('date\tmessage\n')
+        return(f)
+    except:
+        pass
+
+### Append log file
+def append_log_file(input_file_handle):
+
+#    string = '{0}\t{1}\n'.format(datetime.now(), 'sent_alarm_email')
+#    print(string)
+
+    try:
+        input_file_handle.write(
+            '{0}\t{1}\n'.format(
+                time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(time.time())),
+                'sent_alarm_email'
+            )
+        )
+    except:
+        print("Failed to update log file")
+
 ### Get the average temp over the last ten mins
 def get_recent_average(input_df):
     tail = input_df.tail(5)
     average = tail['temp_c'].mean()
     return(average)
+
+### Send alarm email
+def send_email():
+    # Requires you to have initialized your username and password in yagmail:
+    # yagmail.register('palanivelu.lab.freezer@gmail.com', 'password_here')
+    # This stores your credentials in the local systen with Python keyring.
+    yag = yagmail.SMTP('palanivelu.lab.freezer')
+    contents = [
+        "Alert! The freezer has warmed a dangerous amount. See details here:",
+        "https://viz.datascience.arizona.edu/freezer/"
+    ]
+    yag.send('cedardalewarman@gmail.com', 'FREEZER ALARM', contents)
+
+    # Append to log file that you've sent an email
+    log_file = open_log_file()
+    append_log_file(log_file)
+    log_file.close()
 
 ### Main
 def main():
@@ -25,6 +75,10 @@ def main():
     # Getting the average temp over the last 10 mins (readings every 2 mins)
     recent_average = get_recent_average(df)
     print(recent_average)
+
+    # If the temp is greater than -65 then it will send an email. 
+    # Sending email:
+    send_email()
 
 
 
