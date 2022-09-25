4361da542345451a0518dbc624b0da2fded3c599
cedarwarman <cedardalewarman@gmail.com>
2022-09-24 22:02:11 -0700

    Change to Plotly.

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(lubridate)
 library(googlesheets4)
-library(ggplot2)
 library(plotly)
 library(bslib)
 
@@ -21,56 +20,84 @@ import_dataset <- function(sheet_id) {
 
 # This function will make a temperature plot
 make_temp_plot <- function(input_df) {
-  output_plot <- ggplotly(input_df %>%
-    ggplot(aes(date_time, as.numeric(temp_c),
-      # group = 1, # Supposedly necessary in some cases
-      text = paste0("Date and Time: <b>", date_time, "</b>",
-                    "<br>Temperature: <b>", temp_c, " ºC</b>"))) +
-    # geom_smooth(method = "loess", se = FALSE, span = 0.01, color = "white", size = 0.5) +
-    geom_point(size = 2, shape = 16, alpha = 1, color = "blue") +
-    geom_hline(yintercept = -65, size = 1, linetype = "dashed", color = "orange") +
-    scale_y_continuous(breaks = seq(-90, 0, 10),
-                       labels = seq(-90, 0, 10),
-                       limits = c(-90, 0)) +
-    labs(x = "Date and Time", y = " ºC ") +
-    theme_bw() +
-    theme(
-      plot.title = element_blank(),
-      panel.border = element_blank(),
-      panel.grid = element_blank(),
-      axis.line = element_line(size = 2, color = "black"),
-      axis.ticks = element_line(size = 2),
-      axis.text = element_text(size = 12, face = "bold", color = "black"),
-      axis.title = element_text(size = 14, face = "bold")
-    ), tooltip = c("text")) %>%
-    config(displayModeBar = FALSE)
+  # Making plot
+  output_plot <- plot_ly(
+    data = input_df,
+    x = ~date_time,
+    y = as.numeric(input_df$temp_c),
+    type = "scatter",
+    mode= "markers",
+    marker = list(size = 6, color = "magenta")
+  )
+  
+  # Adding aesthetics
+  # This is so clunky but it's how the docs say to do it :/
+  hline <- function(y = 0, color = "cyan") {
+    list(
+      type = "line",
+      x0 = 0,
+      x1 = 1,
+      xref = "paper",
+      y0 = y,
+      y1 = y,
+      layer = "below",
+      line = list(color = color,
+                  dash = "dot",
+                  width = 5)
+    )
+  }
+  
+  output_plot <- output_plot %>% layout(
+    shapes = list(hline(-65)), 
+    xaxis = list(title = list(text = "Date and time",
+                              font = list(size = 28)),
+                 showline = T,
+                 showgrid = F,
+                 linewidth = 1,
+                 linecolor = "white",
+                 tickfont = list(size = 14)),
+    yaxis = list(title = list(text = "ºC",
+                              font = list(size = 28),
+                              standoff = 10),
+                 showline = T,
+                 zeroline = F,
+                 showgrid = F,
+                 linewidth = 1,
+                 linecolor = "white",
+                 tickfont = list(size = 14)),
+    font = list(family = "Arial-BoldMT",
+                color = "white"),
+    paper_bgcolor = "#060606",
+    plot_bgcolor = "#060606",
+    showlegend = F
+  )
+  
+  # Makes the y-axis a little nicer if there's no data that's super warm.
+  if (max(input_df$temp_c) < 0) {
+    output_plot <- output_plot %>% layout(
+      yaxis = list(range = list(-90, 0))
+    )
+  }
+  
+  # Removing toolbar
+  output_plot <- output_plot %>% config(displayModeBar = F) 
   
   return(output_plot)
 }
 
 ui <- bootstrapPage(
-  tags$style(type='text/css', "label { font-size: 28px;
-                                                line-height: 28px;
-                                                font-weight: bold;
-                                                margin-top: 20px; }
-                             .selectize-input { font-size: 28px;
-                                                line-height: 40px;
-                                                font-weight: bold;
-                                                vertical-align: middle;
-                                                text-align: center }
-                             .selectize-dropdown { font-size: 28px;
-                                                   line-height: 40px;
-                                                   font-weight: bold;
-                                                   vertical-align: middle;
-                                                   text-align: center}"),
+  theme  = bs_theme(version = 5,
+                  bootswatch = 'cyborg'),
+  tags$style(type='text/css',
+             "h1 { font-size: calc(1.525rem + 1.5vw); }"), 
   tags$head(tags$link(rel = "shortcut icon", href = "favicon.ico")),
-  theme  = bs_theme(version = 5),
+
   div(class = "container-fluid",
       div(class = "row justify-content-center",
           align = "center",
           div(class = "col-xl-8",
               h1(strong("Freezer temperature")),
-              plotlyOutput("freezer_plot")
+              plotlyOutput("freezer_plot", height = "60vh")
           )
       )
   )
@@ -82,9 +109,7 @@ server <- function(input, output, session) {
   freezer_data <- import_dataset("1KpIEUuMpRD8q3DDNNUeJ1BqSztl_nAzA8DWtdTHFnVY")
   
   # Making the plot
- output$freezer_plot <- renderPlotly({
-    make_temp_plot(freezer_data)
-  })
+  output$freezer_plot <- renderPlotly(make_temp_plot(freezer_data))
 }
 
 shinyApp(ui, server)
